be1c0bb9c00c930bb1e586191c62a9141338b70b
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-06-20 16:01:09 -0700

    AddCategory.jsx Funcionando on submit con enter

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+
+    //value = ['One Punch', 'Dragon Ball']
+    const [ categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+    const onAddCategory = () => {
+        // categories.push('Valorant');
+        setCategories( [ 'Valorant', ...categories ] );
+        //setCategories( cat => [ ...cat, 'Valorant' ]);
+    }
+
+    
+ 
+    console.log(categories);
+ 
   return (
     <>
         {/*Titulo*/}
         <h1>GifExpertApp</h1>
 
         {/*Input*/}
+        <AddCategory />
 
         {/*Listado de gifs*/}
+        <button onClick={ onAddCategory }>Agregar</button>
+        <ol>
+             { categories.map( category => {
+                return <li key={ category }>
+                               { category } 
+                       </li>
+                }) 
+             }
+             {/* <li>ABC</li> */}
+         
+        </ol>
+
+        
             {/*Gif Item*/}
     </>
   )
