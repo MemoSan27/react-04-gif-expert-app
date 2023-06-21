6e55f874a2ebc6e9112c7a60527097e5bcee8aad
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-06-21 15:07:10 -0700

    Archivo de barrill y loading implementado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
 import { useState } from 'react';
-import { AddCategory } from './components/AddCategory';
-import { GifGrid } from './components/GifGrid';
+import { AddCategory, GifGrid } from './components';
+
 
 export const GifExpertApp = () => {
 
    
-    const [ categories, setCategories] = useState(['Dragon Ball']);
+    const [ categories, setCategories] = useState(['friends']);
     
     const onAddCategory = ( newCategory ) => {
         if( categories.includes(newCategory) ) return;
-        
         setCategories( [ newCategory, ...categories ] );
         
     }
